42aaa4d60d67f384fe6f6d6d204e35f09a9ad44b
Worfigoon José de Araújo <Worfigoon@hotmail.com>
2015-07-02 13:34:09 -0300

    commit inicial projeto boletim com egit

---
+++ Boletim/src/br/edu/iftm/boletim/Data.java
@@ -0,0 +1,14 @@
+package br.edu.iftm.boletim;
+
+import java.util.Date;
+
+public class Data {
+
+	public static void main(String[] args) {
+
+    Date data = new Date();
+    
+   System.out.println(data.getYear());
+	}
+
+}
